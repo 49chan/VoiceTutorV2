af640e9ab717fdcd70fec91ffc465d46f1340000
49chan <49chan@naver.com>
2026-07-26 15:47:03 +0900

    Restructure project layout by moving backend modules directly to api directory

--- api/index.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-# Add parent directory to path so that Vercel python resolver can find 'backend' modules
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from backend.main import app
